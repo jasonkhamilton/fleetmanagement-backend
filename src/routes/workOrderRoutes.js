72f8115e9fa9f7a290832f33442d25e2dc31b04d
jasonkhamilton <jason.k.hamilton.96@gmail.com>
2024-02-10 15:10:53 +1100

    Work order images

--- src/routes/workOrderRoutes.js
+++ src/routes/workOrderRoutes.js
@@ -13,10 +13,60 @@ router.post('/', createWorkOrder);
 // router.patch('/:id', partiallyUpdateAsset);
 router.delete('/:id', deleteWorkOrder);
 
-// // Image operations
-// router.get('/image/:id', getAssetImage);
-// router.post('/image/:id', upload.single('image'), uploadAssetImage);
-// router.delete('/image/:id', deleteAssetImage);
+// Image operations
+router.get('/image/:id', getWorkOrderImage);
+router.post('/image/:id', upload.single('image'), uploadWorkOrderImage);
+router.delete('/image/:id', deleteWorkOrderImage);
+
+// Image functions
+async function getWorkOrderImage (req, res) {
+    try {
+        const workOrderId = req.params.id;
+        const query = 'SELECT * FROM work_order_images WHERE work_order_id = $1';
+        const { rows } = await pool.query(query, [workOrderId]);
+        if (rows.length === 0) {
+            return res.status(404).json({ message: 'Work Order Image not found' });
+        }
+        res.json(rows[0]);
+    } catch (err) {
+        console.error('Error fetching work order image', err);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+async function uploadWorkOrderImage (req, res) {
+    try {
+        if (!req.file) {
+            return res.status(400).send('No File Uploaded.');
+        }
+
+        const workOrderId = req.params.id;
+        const imageBinary = req.file.buffer;
+        // const short_description = ???
+
+        const query = 'INSERT INTO work_order_images (work_order_id, image_data) VALUES ($1, $2)';
+        await pool.query(query, [workOrderId, imageBinary]);
+
+        return res.status(200).send('Image Uploaded Successfully.');
+    } catch (err) {
+        console.error('Error uploading work order image', err);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+async function deleteWorkOrderImage (req, res) {
+    try {
+        const query = `DELETE FROM work_order_images WHERE image_id = $1`;
+        const result = await pool.query(query, [req.params.id]);
+        if (result.rowCount === 0) {
+            return res.status(404).json({ message: 'Work order image not deleted.' });
+        } else {
+            return res.status(200).json({ message: 'Work order image deleted successfully.' });
+        }
+    } catch (err) {
+        res.status(500).send('Internal Server Error');
+    } 
+}
 
 async function getAllWorkOrders (req, res) {
     try {
